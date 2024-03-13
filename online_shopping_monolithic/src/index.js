7ad940e7335b924ffbb52efe9206cea1d94deea5
anessrb <anesshiba@gmail.com>
2024-03-13 20:53:29 +0100

    first commit

--- online_shopping_monolithic/src/index.js
+++ online_shopping_monolithic/src/index.js
@@ -3,21 +3,22 @@ const { PORT } = require('./config');
 const { databaseConnection } = require('./database');
 const expressApp = require('./express-app');
 
-const StartServer = async() => {
-
+const StartServer = async () => {
     const app = express();
     
     await databaseConnection();
     
     await expressApp(app);
 
-    app.listen(PORT, () => {
-        console.log(`listening to port ${PORT}`);
+    const customPort = 8000; // Define your custom port here
+    
+    app.listen(customPort, () => {
+        console.log(`listening to port ${customPort}`);
     })
     .on('error', (err) => {
         console.log(err);
         process.exit();
-    })
+    });
 }
 
 StartServer();
